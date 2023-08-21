1aa30ec636d0c36be294683bd86b743960ccd8ff
UmerBaig123 <muhammadumerbaig9922@gmail.com>
2023-08-21 15:52:59 +0500

    adds fonts to the main appjs

--- App.js
+++ App.js
@@ -2,13 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View ,Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
+import * as Font from "expo-font";
+import Apploading from "expo-app-loading";
 
+const getFonts = () =>
+  Font.loadAsync({
+    Techno: require("./assets/fonts/ShareTechMono-Regular.ttf"),
+    Orbitron: require("./assets/fonts/Orbitron-VariableFont_wght.ttf"),
+    Digital: require("./assets/fonts/digital.ttf"),
+    SCode: require('./assets/fonts/SourceCodePro-BoldItalic.ttf')
+  });
 
 
 import MainScreen from './screens/MainScreen';
 
 const Stack = createNativeStackNavigator()
 export default function App() {
+  const [fontsloaded, setFontsLoaded] = useState(false);
+  if (fontsloaded) {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -31,17 +43,26 @@ export default function App() {
             title: 'Calorie Counter',
              headerTitleStyle:{
               color:"#6495ED",
-              fontWeight:"900",
+              fontFamily:"Digital",
               flex:1,
               flexdirection:"row",
-              fontSize:25,
-               fontFamily:"sans-serif",
+              fontSize:30,
           },statusBarColor:"#000000",}}
         />
         </Stack.Group>
         </Stack.Navigator>
     </NavigationContainer>
-  );
+  );}else{
+    return (
+      <Apploading
+        startAsync={getFonts}
+        onFinish={() => {
+          setFontsLoaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
