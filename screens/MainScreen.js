5ebd433d56b10929e8ba1698d26eace8007829f4
UmerBaig123 <muhammadumerbaig9922@gmail.com>
2023-08-24 12:08:37 +0500

    Adds prettier formatting

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,76 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { TextInput,StyleSheet, Text, View, Dimensions } from 'react-native';
-import CustomButton from '../components/CustomButton';
-import { useState } from 'react';
-import { Formik } from 'formik';
-import CustomInput from '../components/CustomInput';
-import AppSingleDropdown from '../components/CustomDropdown';
-import GenderSelector from '../components/GenderSelector';
-import calculateBMI from '../functions/CalculateBMI';
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import CustomButton from "../components/CustomButton";
+import { useState } from "react";
+import { Formik } from "formik";
+import CustomInput from "../components/CustomInput";
+import AppSingleDropdown from "../components/CustomDropdown";
+import GenderSelector from "../components/GenderSelector";
+import calculateBMI from "../functions/CalculateBMI";
 
-const MainScreen=({navigation})=>{
-  const colors = ["#ffff99","#cc9900","#99cc00","#66ff33","#00ffff","#0066cc"]
+const MainScreen = ({ navigation }) => {
+  const colors = [
+    "#ffff99",
+    "#cc9900",
+    "#99cc00",
+    "#66ff33",
+    "#00ffff",
+    "#0066cc",
+  ];
   const placeholders = [
     "Select your daily activity ",
     "Very Low Activity ",
     "Low Activity ",
     "Moderate Activity ",
     "High Activity ",
-    "Very High Activity "
-  ]
+    "Very High Activity ",
+  ];
   const placeholdersG = [
     "Select your Goal ",
     "Lose Weight Fast ",
     "Lose Weight Slowly ",
     "Maintain Weight ",
     "Gain Weight Slowly ",
-    "Gain Weight Fast "
-  ]
-  const [placeholder,setPlaceholder] = useState(placeholders[0])
-  const [placeholderG,setPlaceholderG] = useState(placeholdersG[0])
-  const [ColorG,setColorG] = useState(colors[0])
-  const [color,setColor] = useState(colors[0])
-  const [wUnit,setWUnit] = useState('Weight (LBS)')
-  const windowWidth = Dimensions.get('window').width;
+    "Gain Weight Fast ",
+  ];
+  const [placeholder, setPlaceholder] = useState(placeholders[0]);
+  const [placeholderG, setPlaceholderG] = useState(placeholdersG[0]);
+  const [ColorG, setColorG] = useState(colors[0]);
+  const [color, setColor] = useState(colors[0]);
+  const [wUnit, setWUnit] = useState("Weight (LBS)");
+  const [hUnit, setHUnit] = useState("Height (Ft/In)");
+  const [Ft, setFt] = useState(true);
+  const windowWidth = Dimensions.get("window").width;
   const windowheight = Dimensions.get("window").height;
-  const [error, setError] = useState('')
-    
-    return(
-        <View style={styles.container}>
+  const [error, setError] = useState("");
 
-          <Formik
-            initialValues={{ 
-              weight: '',
-              heightFt:'',
-              heightIn:'',
-              Activity:0,
-              Gender:"",
-              Goal:"",
-              weightUnit:"LBS",
-             }}
-            onSubmit={(values) =>{
-               if(values.Activity==""||values.Gender==""||values.Goal==""||values.heightFt==""||values.heightIn==""||values.weight==""){
-                let error = ''
-                for (let key in values) {
-                  if (values[key] === "") {
-                    error = error?error+", "+key:error+key;
-                  }
-                }
-                setError(error+" cannot be empty")
-               }else{
-                calculateBMI(values)
-                console.log(values)
-               }
-              }}
-          >
+  return (
+    <View style={styles.container}>
+      <Formik
+        initialValues={{
+          weight: "",
+          heightFt: "",
+          heightIn: "",
+          Activity: 0,
+          Gender: "",
+          Goal: "",
+          weightUnit: "LBS",
+          heightUnit: Ft,
+        }}
+        onSubmit={(values) => {
+          if (Ft) {
+            values.heightIn == "00";
+          }
+          if (
+            values.Activity == "" ||
+            values.Gender == "" ||
+            values.Goal == "" ||
+            values.heightFt == "" ||
+            values.heightIn == "" ||
+            values.weight == ""
+          ) {
+            let error = "";
 
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            for (let key in values) {
+              if (values[key] === "") {
+                error = error ? error + ", " + key : error + key;
+              }
+            }
+            setError(error + " cannot be empty");
+          } else {
+            calculateBMI(values);
+            navigation.navigate("BMIScreen");
+          }
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
           <View>
-
-          <CustomInput
-          inputmode={"numeric"}
-          title={wUnit}
-          multipleUnits={true}
+            <CustomInput
+              inputmode={"numeric"}
+              title={wUnit}
+              multipleUnits={true}
               onChangeText={handleChange("weight")}
               onBlur={handleBlur("weight")}
               setUnit={(val) => {
@@ -79,51 +96,51 @@ const MainScreen=({navigation})=>{
               }}
               unitOnPress={() => {
                 console.log("hi");
-          }}
-          value={values.weight}
+              }}
+              value={values.weight}
               unit={["LBS", "KG"]}
-          />
+            />
 
             {Ft ? (
               <View
                 style={{
                   paddingBottom: windowheight * 0.009,
-            flexDirection: "row",
+                  flexDirection: "row",
                   alignContent: "center",
                 }}
               >
-          <CustomInput
-          inputmode={"numeric"}
+                <CustomInput
+                  inputmode={"numeric"}
                   title={hUnit}
-          titleAlign={"flex-end"}
+                  titleAlign={"flex-end"}
                   width={windowWidth * 0.499}
                   onChangeText={handleChange("heightFt")}
                   onBlur={handleBlur("heightFt")}
-          value={values.heightFt}
-          unit="Ft"
+                  value={values.heightFt}
+                  unit="Ft"
                   unitOnPress={() => {
                     setHUnit("Height (Meters)");
                     values.heightUnit = false;
                     setFt(false);
                   }}
-          />
+                />
 
-          <CustomInput
-          inputmode={"numeric"}
-          title="."
+                <CustomInput
+                  inputmode={"numeric"}
+                  title="."
                   paddingLeft={windowWidth * 0.7}
                   onChangeText={handleChange("heightIn")}
                   onBlur={handleBlur("heightIn")}
-          value={values.heightIn}
+                  value={values.heightIn}
                   width={windowWidth * 0.499}
-          unit="In"
+                  unit="In"
                   unitOnPress={() => {
                     setHUnit("Height (Meters)");
                     values.heightUnit = false;
                     setFt(false);
                   }}
-          />
-          </View>
+                />
+              </View>
             ) : (
               <CustomInput
                 inputmode={"numeric"}
@@ -140,165 +157,233 @@ const MainScreen=({navigation})=>{
               />
             )}
 
+            <View
+              style={{
+                height: windowheight * 0.1,
+                flexDirection: "column",
+              }}
+            >
+              <GenderSelector
+                setFieldValue={(val) => {
+                  values.Gender = val;
+                }}
+              />
+            </View>
+            <View
+              style={{
+                paddingTop: windowheight * 0.009,
+              }}
+            >
+              <AppSingleDropdown
+                title={"Activity"}
+                color={color}
+                placeholder={placeholder}
+                options={[
+                  {
+                    value: 1,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#cc9900",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Very Low Activity
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 2,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#99cc00",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Low Activity
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 3,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#66ff33",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Moderate Activity
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 4,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#00ffff",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        High Activity
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 5,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#0066cc",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Very High Activity
+                      </Text>
+                    ),
+                  },
+                ]}
+                value={values["Activity"]}
+                name="Activity"
+                onChange={(val) => {
+                  setColor(colors[val]);
+                  setPlaceholder(placeholders[val]);
+                  values.Activity = val;
+                }}
+              />
+              <AppSingleDropdown
+                title={"Goal"}
+                color={ColorG}
+                placeholder={placeholderG}
+                options={[
+                  {
+                    value: 1,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#cc9900",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Lose Weight Fast
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 2,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#99cc00",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Lose Weight Slowly
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 3,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#66ff33",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Maintain Weight
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 4,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#00ffff",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Gain Weight Slowly
+                      </Text>
+                    ),
+                  },
+                  {
+                    value: 5,
+                    label: (
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#0066cc",
+                          fontFamily: "SCode",
+                        }}
+                      >
+                        Gain Weight Fast
+                      </Text>
+                    ),
+                  },
+                ]}
+                value={values["Goal"]}
+                name="Goal"
+                onChange={(val) => {
+                  setColorG(colors[val]);
+                  setPlaceholderG(placeholdersG[val]);
+                  values.Goal = val;
+                }}
+              />
+              <Text style={styles.errText}>{error}</Text>
+            </View>
+            <View
+              style={{
+                paddingTop: windowWidth * 0.428,
+              }}
+            >
+              <CustomButton
+                onPress={handleSubmit}
+                title="Submit"
+                icon={"check-all"}
+              />
+            </View>
           </View>
-          <View style={{
-            paddingTop: windowheight*0.009,
-          }}>
-
-          <AppSingleDropdown
-          title={"Activity"}
-          color={color}
-          placeholder= {placeholder}
-          options={[
-            { value: 1, label: (
-             <Text
-             style={{
-              fontSize:16,
-              color:"#cc9900",
-              fontFamily:"SCode",
-             }}
-             >Very Low Activity</Text> 
-            )},
-            { value: 2, label: (
-              <Text
-              style={{
-                fontSize:16,
-               color:"#99cc00",
-               fontFamily:"SCode",
-              }}
-              >Low Activity</Text> 
-             ) },
-            { value: 3, label:(
-              <Text
-              style={{
-                fontSize:16,
-               color:"#66ff33",
-               fontFamily:"SCode",
-              }}
-              >Moderate Activity</Text> 
-             ) },
-            { value: 4, label: (
-              <Text
-              style={{
-                fontSize:16,
-               color:"#00ffff",
-               fontFamily:"SCode",
-              }}
-              >High Activity</Text> 
-             ) },
-            { value: 5, label: (
-              <Text
-              style={{
-                fontSize:16,
-               color:"#0066cc",
-               fontFamily:"SCode",
-              }}
-              >Very High Activity</Text> 
-             ) }
-          ]}
-          value={values["Activity"]}
-          name="Activity"
-          onChange={(val)=>{
-            setColor(colors[val])
-            setPlaceholder(placeholders[val])
-            values.Activity=val}}
-        />
-          <AppSingleDropdown
-          title={"Goal"}
-          color={ColorG}
-          placeholder= {placeholderG}
-          options={[
-            { value: 1, label: (
-             <Text
-             style={{
-              fontSize:16,
-              color:"#cc9900",
-              fontFamily:"SCode",
-             }}
-             >Lose Weight Fast</Text> 
-            )},
-            { value: 2, label: (
-              <Text
-              style={{
-                fontSize:16,
-               color:"#99cc00",
-               fontFamily:"SCode",
-              }}
-              >Lose Weight Slowly</Text> 
-             ) },
-            { value: 3, label:(
-              <Text
-              style={{
-                fontSize:16,
-               color:"#66ff33",
-               fontFamily:"SCode",
-              }}
-              >Maintain Weight</Text> 
-             ) },
-            { value: 4, label: (
-              <Text
-              style={{
-                fontSize:16,
-               color:"#00ffff",
-               fontFamily:"SCode",
-              }}
-              >Gain Weight Slowly</Text> 
-             ) },
-            { value: 5, label: (
-              <Text
-              style={{
-                fontSize:16,
-               color:"#0066cc",
-               fontFamily:"SCode",
-              }}
-              >Gain Weight Fast</Text> 
-             ) }
-          ]}
-          value={values["Goal"]}
-          name="Goal"
-          onChange={(val)=>{
-            setColorG(colors[val])
-            setPlaceholderG(placeholdersG[val])
-            values.Goal=val}}
-        />
-        <Text style={styles.errText}>{error}</Text>
-        </View>
-        <View style={{
-          paddingTop:windowWidth*0.428,
-        }}>
-            <CustomButton onPress={handleSubmit} title="Submit" icon={"check-all"}/>
-        </View>
-       </View>
-     )}
-   </Formik>
-          
-        </View>
-    )
-}
+        )}
+      </Formik>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   errText: {
-    textAlign:"center",
-    color:"#FF0000",
-    fontFamily: 'Glitch',
-    fontSize:12,
-  },  
-    input:{
-      height:50,
-     textAlign: 'center',
-     backgroundColor: "#e6ffff",
-     borderColor: '#808080',
-     borderWidth: 1,
-     color:"#6495ED",
-     fontWeight:"700",
-     margin:15,
-     fontSize:1,
-     fontFamily:"sans-serif",
-    },
-    container: {
-      
-      flex: 1,
-      backgroundColor: '#ccffff',
-    },
-  });  
+    textAlign: "center",
+    color: "#FF0000",
+    fontFamily: "Glitch",
+    fontSize: 12,
+  },
+  input: {
+    height: 50,
+    textAlign: "center",
+    backgroundColor: "#e6ffff",
+    borderColor: "#808080",
+    borderWidth: 1,
+    color: "#6495ED",
+    fontWeight: "700",
+    margin: 15,
+    fontSize: 1,
+    fontFamily: "sans-serif",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#ccffff",
+  },
+});
 export default MainScreen;
