c325bfc94f1afd7f861066dd9043267ba4a7c1ab
UmerBaig123 <muhammadumerbaig9922@gmail.com>
2023-08-24 12:06:46 +0500

    Adds new screen FoodScreen (empty)

---
+++ screens/FoodScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const FoodScreen = () => {
+  return <View style={styles.container}>{/* Your content goes here */}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default FoodScreen;
